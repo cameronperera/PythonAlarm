4ec17d9691b407133914c3915e90aff29b0180ef
Cameron Perera <cameronrperera@gmail.com>
2017-10-05 12:43:43 -0500

    Create Alarm.py

---
+++ Alarm.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python3
+from datetime import datetime
+import time
+import os
+import random
+import sys
+import numpy
+import json
+from tkinter import *
+import winsound
+
+
+punch = []
+saturday = 5
+
+music = 'Various-04.wav'
+windowText = 'Punch!'
+windowImage = 'vader_business.gif'
+
+def alarmWindow():
+    root = Tk()
+    root.title('Alarm Clock')
+    root.geometry('920x640')
+    app = Frame(root)
+    app.grid()
+    filename = PhotoImage(file = windowImage)
+    background_label = Label(app, image=filename)
+    background_label.grid()
+    winsound.PlaySound(music, winsound.SND_ALIAS | winsound.SND_ASYNC)
+    text = Canvas(app, width=150, height=75)
+    text.create_text(75, 35, font='Times 32 bold', text= windowText)
+    text.grid(row=0, column=0, sticky='nw')
+
+    root.mainloop()
+
+def convertToList(punch):
+    intPunch = punch.split(':')
+    intList = list(map(int, intPunch))
+    return intList
+
+def currentTimeArray():
+    currentTimeHour = datetime.now().hour
+    currentTimeMinute = datetime.now().minute
+    currentTimeArray = [currentTimeHour, currentTimeMinute]
+    return currentTimeArray
+
+def substractPunchFromCurrent(currentTime, punchTime):
+    currentTime = numpy.array(currentTime)
+    punchTime = numpy.array(punchTime)
+    subtractedTime = punchTime - currentTime
+    if subtractedTime[0] < 0:
+        subtractedTime = subtractedTime + 24
+    return subtractedTime
+
+def secondsToSleep(subtractedTime):
+    secondsSleep = (subtractedTime[0]*3600 + subtractedTime[1]*60)
+    convertSleepToInt = int(secondsSleep)
+    return convertSleepToInt
+
+def convertPMTimeTo24Format(punch):
+    punch[0] = punch[0] + 12
+    return punch
+
+def checkIfFileExist(fileCheck):
+    if os.path.isfile(fileCheck):
+        return True
+    else:
+        return False
+
+def removeTimeChar(inputPunch, char):
+    for i in char:
+        inputPunch = inputPunch.replace(i, '')
+    return inputPunch
+
+def amPunchConversion(amPunch):
+    amPunch = removeTimeChar(amPunch, ' am')
+    amPunch = convertToList(amPunch)
+    return amPunch
+
+def pmPunchConversion(pmPunch):
+    pmPunch = removeTimeChar(pmPunch, ' pm')
+    pmPunch = convertToList(pmPunch)
+    return pmPunch
+
+def noonConversion(noon):
+    noonPunch = noon
+    noonPunch = removeTimeChar(noonPunch, ' pm')
+    noonPunch = convertToList(noonPunch)
+    return noonPunch
+
+def collectNewPunchTimes(punches):
+    inputPunchStr = punches
+    if 'am' in inputPunchStr:
+        inputPunch = amPunchConversion(inputPunchStr)
+        punch.append(inputPunch)
+    elif 'pm' in inputPunchStr:
+        if '12' in inputPunchStr:
+            inputPunch = noonConversion(inputPunchStr)
+        else:
+            inputPunch = pmPunchConversion(inputPunchStr)
+            inputPunch = convertPMTimeTo24Format(inputPunch)
+        punch.append(inputPunch)
+
+def putProgramToSleep(secondsToSleep):
+    time.sleep(secondsToSleep)
+
+def getStoredPunches():
+    fileCheck = checkIfFileExist('punchTimes.txt')
+    if fileCheck == True:
+        fileData = openAndReadFile('punchTimes.txt')
+        return fileData
+    else:
+        return fileCheck
+
+def openAndReadFile(fileName):
+    with open(fileName) as jsonFile:
+        data = json.load(jsonFile)
+    return data
+
+def writeToFile(fileName, punchTimes):
+    with open(fileName, 'w') as jsonfile:
+        json.dump(punchTimes, jsonfile)
+
+def start():
+    whileVar = True
+    global punch
+    while whileVar == True:
+        fileCheck = input('Would you like to enter "new" punch times or use "stored"? ')
+        if fileCheck == 'new':
+            while whileVar == True:
+                inputPunchStr = input('Please enter one punch time (ex. 8:30am or 5:00pm) or end to stop: ')
+                if inputPunchStr == 'end':
+                    whileVar = False
+                else:
+                    collectNewPunchTimes(inputPunchStr)
+                writeToFile('punchTimes.txt', punch)
+            punch = getStoredPunches()
+            return punch
+        elif fileCheck == 'stored':
+            checkFile = checkIfFileExist('punchTimes.txt')
+            if checkFile == True:
+                punch = getStoredPunches()
+                whileVar = False
+                return punch
+            else:
+                print('No stored punch times, please create new punch times.')
+        else:
+            print('Plese enter "new" or "stored".')
+
+def selectPunchTime(punch):
+    currentTime = currentTimeArray()
+    punchInitial = punch
+    for b in punchInitial:
+        if currentTime < b:
+            selectedPunch = b
+            break
+        else:
+            selectedPunch = punchInitial[0]
+    return selectedPunch
+
+
+alarm = start()
+
+while True:
+    dayNumber = datetime.today().weekday()
+
+    if dayNumber == saturday:
+        time.sleep(sleep2days)
+    else:
+        presentTime = currentTimeArray()
+        print('The present time is ' + str(presentTime))
+        nextPunch = selectPunchTime(alarm)
+        print('The next selected punch time is ' + str(nextPunch))
+        timeDifference = substractPunchFromCurrent(presentTime, nextPunch)
+        sleepSeconds = secondsToSleep(timeDifference)
+        print('Seconds that the program will sleep is ' + str(sleepSeconds))
+        print('')
+        putProgramToSleep(sleepSeconds)
+        alarmWindow()
